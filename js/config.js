8d13d2ef560c592d3ba46921c6296b0a028eaee7
koochangson <koochangson@gmail.com>
2026-07-09 18:08:37 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
 window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxkkDaq71UOUSsAshBYAifScXbHDoETOLmK039qbiHaPrtvZeu62kumnXWl1IcyLQos/exec';
 
 // Google Cloud Console에서 발급한 OAuth 2.0 클라이언트 ID (apps-script/Config.gs와 동일한 값)
-window.GOOGLE_CLIENT_ID = '1qmKX0kAIF-zlwbm14jFGysszeA3zTrUAdKlmP7dFk78.apps.googleusercontent.com';
+window.GOOGLE_CLIENT_ID = '1010535602579-u59sj3ffmvvqp0qm37o0c2p1f0nel7pk.apps.googleusercontent.com';
